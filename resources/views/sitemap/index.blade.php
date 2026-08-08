22afc06276f11ebed1e1cfbb84a10d930d761cf0
Yoda <tdrabik@developerzone.eu>
2026-08-08 21:23:43 +0100

    style: pre-existing concat_space in the sitemap view

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,4 +1,4 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n"; ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'."\n"; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach($urls as $url)
     <url>
