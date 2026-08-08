527fa613b9c04bfc8e73e2355276d4ee652cba4a
rodney93 <rhoanneyacud@gmail.com>
2026-08-08 21:07:57 +0800

    feat(modules): initial scaffolding for Core, Filament, Livewire, and Api modules

---
+++ app/Modules/Api/ApiModule.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Modules\Api;
+
+use App\Modules\BaseModule;
+
+class ApiModule extends BaseModule
+{
+    protected function onEnable(): void
+    {
+        // Called when module is enabled
+    }
+
+    protected function onDisable(): void
+    {
+        // Called when module is disabled
+    }
+
+    protected function onInstall(): void
+    {
+        // Called when module is installed
+    }
+
+    protected function onUninstall(): void
+    {
+        // Called when module is uninstalled
+    }
+}
